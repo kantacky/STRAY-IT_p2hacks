e34b0ab5a01d3f1872b014c98f674ce2ff90af92
Kanta Oikawa <k.oikawa@kantacky.com>
2022-12-11 11:37:03 +0900

    Initial Commit

---
+++ STRAY-IT/STRAY_ITApp.swift
@@ -0,0 +1,17 @@
+//
+//  STRAY_ITApp.swift
+//  STRAY-IT
+//
+//  Created by Kanta Oikawa on 2022/12/11.
+//
+
+import SwiftUI
+
+@main
+struct STRAY_ITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
