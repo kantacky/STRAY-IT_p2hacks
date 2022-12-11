2854271253fe916235e71ee92836f80568fbd5db
Kanta Oikawa <k.oikawa@kantacky.com>
2022-12-11 12:54:29 +0900

    [Add] Add DirectionView

---
+++ STRAY-IT/Views/Direction/DirectionView.swift
@@ -0,0 +1,20 @@
+//
+//  DirectionView.swift
+//  STRAY-IT
+//
+//  Created by Kanta Oikawa on 2022/12/11.
+//
+
+import SwiftUI
+
+struct DirectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DirectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        DirectionView()
+    }
+}
