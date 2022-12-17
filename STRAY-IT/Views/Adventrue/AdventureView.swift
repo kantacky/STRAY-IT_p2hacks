4767dfc3078f645d4a8e5c5ab2b0b152e2a32544
Kanta Oikawa <k.oikawa@kantacky.com>
2022-12-17 09:51:28 +0900

    [Add] Add AdventureView

---
+++ STRAY-IT/Views/Adventrue/AdventureView.swift
@@ -0,0 +1,20 @@
+//
+//  AdventureView.swift
+//  STRAY-IT
+//
+//  Created by Kanta Oikawa on 2022/12/17.
+//
+
+import SwiftUI
+
+struct AdventureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdventureView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdventureView()
+    }
+}
